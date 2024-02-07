bdce093117285579eff5f2eca7940b9f2e15ad57
benoitAD <benoit.augerdubois@gmail.com>
2024-02-07 15:40:05 +0100

    Init project

---
+++ analyseBesoin/src/main/java/com/espi/analyseBesoin/ExoPorteApplication.java
@@ -0,0 +1,13 @@
+package com.espi.analyseBesoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoPorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoPorteApplication.class, args);
+	}
+
+}
---
+++ analyseBesoin/src/test/java/com/espi/analyseBesoin/ExoPorteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espi.analyseBesoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoPorteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
