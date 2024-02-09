ade19e1a398f2a0f05725cbf14e43b36968a8d83
debian-kristen <debian-kristen@serveur-mail.kristen>
2024-02-09 13:55:52 +0100

    ajout de la fonctionnalité associé à casAttribuerBadge  => le test passe au vert

--- analyseBesoin/src/main/java/com/espi/analyseBesoin/ControleAccess/MoteurOuverture.java
+++ analyseBesoin/src/main/java/com/espi/analyseBesoin/ControleAccess/MoteurOuverture.java
@@ -1,11 +1,10 @@
 package com.espi.analyseBesoin.ControleAccess;
 
-import com.espi.analyseBesoin.ControleAccess.IPorte;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MoteurOuverture implements IPorte, ILecteur {
     private IPorte[] portes;
+    private Badge[] badgesAssocies = new Badge[0];
 
     public MoteurOuverture(IPorte... portes) {
         this.portes = portes;
@@ -27,5 +26,17 @@ public class MoteurOuverture implements IPorte, ILecteur {
     @Override
     public void Ouvrir() {
 
+    }
+
+    public boolean possede(Badge badge) {
+
+        return Arrays.stream(this.badgesAssocies).filter(b -> b.equals(badge)).count() == 1;
+    }
+
+    public void associer(Badge badge) {
+        Badge[] badgesMiseAJour = new Badge[this.badgesAssocies.length + 1];
+        System.arraycopy(this.badgesAssocies, 0, badgesMiseAJour, 0, this.badgesAssocies.length);
+        badgesMiseAJour[this.badgesAssocies.length] = badge;
+        this.badgesAssocies = badgesMiseAJour;
     }
 }
